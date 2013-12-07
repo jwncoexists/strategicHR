e37bb3f0e9433200ed32a2a03395a3a729f09f8b
Jennifer Nelson <jenniferwnelson@sbcglobal.net>
2013-12-07 11:22:13 -0800

    Certificate updates

--- app/pdfs/certificate_pdf.rb
+++ app/pdfs/certificate_pdf.rb
@@ -9,8 +9,28 @@ class CertificatePdf < Prawn::Document
   end
   
   def certificate_info
-    text_box "#{@user.name}", at: [0,400], width: 720, height: 36, size: 24, align: :center, overflow: :shrink_to_fit
-    text_box  "#{@course.name}",  at: [0,300], width: 720, height: 36, size: 24, align: :center, overflow: :shrink_to_fit
+    font "Helvetica", style: :bold_italic, color: "rgb=003399"
+    text_box "#{@user.name}",
+              at: [0,400],
+              width: 720, 
+              height: 36, 
+              size: 24, 
+              align: :center, 
+              overflow: :shrink_to_fit
+    text_box  "#{@course.name}",  
+              at: [0,300],
+              width: 720, 
+              height: 36, 
+              size: 24, 
+              align: :center, 
+              overflow: :shrink_to_fit
+    text_box  "#{@certificate.purchase_date.strftime('%B %e, %Y')}",  
+              at: [110,198],
+              width: 200, 
+              height: 36, 
+              size: 18, 
+              align: :center, 
+              overflow: :shrink_to_fit
     #text "On #{@certificate.purchase_date}"
   end
 end
