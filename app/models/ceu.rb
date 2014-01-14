7f5ef386ad4c7e862922b5397fa7a2fba41216c8
Jennifer Nelson <jenniferwnelson@sbcglobal.net>
2014-01-14 11:25:11 -0800

    Updated so that course # and CEU credits print out on certificate

--- app/models/ceu.rb
+++ app/models/ceu.rb
@@ -1,7 +1,11 @@
 class Ceu < ActiveRecord::Base
   belongs_to :course
+  belongs_to :certificate
 
   def full_ceu_name
     "#{self.credit} #{self.name} Credit".pluralize(self.credit) + " - #{self.organization}"
+  end
+  def full_ceu_name_no_org
+    "#{self.credit} #{self.name} Credit".pluralize(self.credit)
   end
 end
--- app/pdfs/certificate_pdf.rb
+++ app/pdfs/certificate_pdf.rb
@@ -3,35 +3,57 @@ class CertificatePdf < Prawn::Document
   def initialize(certificate)
     super(page_layout: :landscape)
     @certificate = certificate
+    @ceu = Ceu.find(@certificate.ceu_id)
     @user = User.find(@certificate.user_id)
     @course = Course.find(@certificate.course_id)
-    image "#{Rails.root}/app/assets/images/StrategicHRCertificate.png", :at => [0, 612]
+    image "#{Rails.root}/app/assets/images/StrategicHRCertificate.png", :at => [0, 542]
     certificate_info
   end
   
   def certificate_info
-    font "Helvetica", style: :bold_italic, color: "rgb=003399"
+    font "Helvetica", style: :bold_italic
+    stroke_color "003399"
+    fill_color "003399"
     text_box "#{@user.name}",
-              at: [0,400],
+              at: [0,350],
               width: 720, 
               height: 36, 
               size: 24, 
-              align: :center, 
+              align: :center,
+              color: [100,0,0,0],
               overflow: :shrink_to_fit
-    text_box  "#{@course.name}",  
-              at: [0,300],
+    font "Helvetica", style: :italic
+    if( @ceu )
+      text_box  "# #{@ceu.code} #{@course.name}",  
+              at: [0,270],
               width: 720, 
               height: 36, 
-              size: 24, 
+              size: 18, 
               align: :center, 
               overflow: :shrink_to_fit
+      text_box  "#{@ceu.full_ceu_name_no_org}",  
+              at: [0,240],
+              width: 720, 
+              height: 36, 
+              size: 18, 
+              align: :center, 
+              overflow: :shrink_to_fit
+    else
+      text_box  "#{@course.name}",  
+              at: [0,250],
+              width: 720, 
+              height: 36, 
+              size: 20, 
+              align: :center, 
+              overflow: :shrink_to_fit
+    end
     text_box  "#{@certificate.purchase_date.strftime('%B %e, %Y')}",  
-              at: [110,198],
+              at: [78,163],
               width: 200, 
               height: 36, 
               size: 18, 
               align: :center, 
               overflow: :shrink_to_fit
-    #text "On #{@certificate.purchase_date}"
+
   end
 end
