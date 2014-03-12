d8e942dd51b1b0e7dece2fd4942adc95027fc56e
Jennifer Nelson <jenniferwnelson@sbcglobal.net>
2014-03-11 21:05:30 -0700

    Updated cloud55 script to preserve resource images across builds.

--- .cloud66/handouts.sh
+++ .cloud66/handouts.sh
@@ -6,3 +6,7 @@ mkdir -p $STACK_BASE/shared/uploads/course/image
 chown nginx:app_writers $STACK_BASE/shared/uploads/course/image
 rm -rf $STACK_PATH/public/uploads/course/image
 ln -nsf $STACK_BASE/shared/uploads/course/image $STACK_PATH/public/uploads/course/image
+mkdir -p $STACK_BASE/shared/uploads/resource/image
+chown nginx:app_writers $STACK_BASE/shared/uploads/resource/image
+rm -rf $STACK_PATH/public/uploads/resource/image
+ln -nsf $STACK_BASE/shared/uploads/resource/image $STACK_PATH/public/uploads/resource/image
