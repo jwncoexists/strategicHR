bfd6e5918fda2e81dd65a7db648f5a38389a75d4
Jennifer Nelson <jenniferwnelson@sbcglobal.net>
2014-03-15 19:13:41 -0700

    Updated reports.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,12 +1,17 @@
 class ReportsController < ApplicationController
   def index
     @courses = Course.all
-    @course_data = []
-    @course_data << ['Course', 'Certificates', 'Video Minutes Watched', 
-                    'Quizzes Passed', 'Quizzes Failed']
+    @video_data = []
+    @video_data << ['Course', 'Video Minutes Watched']
+    @certificate_data = []
+    @certificate_data << ['Course', 'Certificates']
+    @quiz_data = []
+    @quiz_data << ['Course', 'Quizzes Passed', 'Quizzes Failed']
+
     @courses.each do |course|
-      @course_data << [course.name, course.certificate_count, course.video_minutes_total,
-                       course.quizzes_passed_count, course.quizzes_failed_count]
+      @video_data << [course.name, course.video_minutes_total]
+      @certificate_data << [course.name, course.certificate_count]
+      @quiz_data << [course.name, course.quizzes_passed_count, course.quizzes_failed_count]
     end
   end
 
